60b92e4c69bf606dd3089f7e22864205bfdef333
maheng <heng.ma@sabegeek.com>
2026-09-16 10:02:46 +0800

    修复webflux环境下，多数据源不加载的问题

--- spring-boot-starter-jdbc/src/main/java/io/github/opensabe/jdbc/autoconfigure/JdbcAutoConfiguration.java
+++ spring-boot-starter-jdbc/src/main/java/io/github/opensabe/jdbc/autoconfigure/JdbcAutoConfiguration.java
@@ -4,11 +4,14 @@ import io.github.opensabe.jdbc.autoconfigure.config.ConverterConfiguration;
 import io.github.opensabe.jdbc.autoconfigure.config.DynamicDataSourceConfiguration;
 import io.github.opensabe.jdbc.autoconfigure.config.GenerateConfiguration;
 import io.github.opensabe.jdbc.autoconfigure.config.ObservationConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * @author heng.ma
  */
+@AutoConfiguration(before = DataSourceAutoConfiguration.class)
 @Import({
         GenerateConfiguration.class,
         DynamicDataSourceConfiguration.class,
